b7e37c3f9a0175e8561a03d4d7cf95f3a06afc62
Yahia Jabeur <yahia.jabeur@gmail.com>
2024-12-10 15:10:17 +0100

    add image attribute to recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_10_130753) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_10_140701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_10_130753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "search_id", null: false
+    t.string "image"
     t.index ["search_id"], name: "index_recipes_on_search_id"
   end
 
