42243ce346dc45545a46d5ee1e20ad4382580836
Esha Singh <singh.esha99999@gmail.com>
2024-12-11 12:15:09 +0100

    saved-favourite-recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   has_one_attached :stored_image
-
+  has_many :favorites, dependent: :destroy
+  has_many :users_who_favorited, through: :favorites, source: :user
+  has_many :favorited_by, through: :favorites, source: :user
+  validates :name, presence: true
+  validates :rating, inclusion: { in: 1..5 }, allow_nil: true
+  validates :search_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :searches, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_recipes, through: :favorites, source: :recipe
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
