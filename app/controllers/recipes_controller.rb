dcc800769f921d5d50561d98f399133aa4822218
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2024-12-12 10:48:36 +0100

    unfinished recipe API integration

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,32 +8,45 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
+    api_key = ENV['SPOONACULAR_ACCESS_TOKEN']
+    ingredients = params[:ingredients]
+    url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=#{ingredients}&number=10&ranking=1&apiKey=#{api_key}"
+
+    begin
+      # Open the URL and parse the JSON response
+      response = URI.open(url).read
+      @posts = JSON.parse(response)
+    rescue OpenURI::HTTPError => e
+      # Handle errors (e.g., API down, invalid URL)
+      @posts = []
+      flash[:alert] = "Failed to fetch posts: #{e.message}"
+    end
   end
 
   def show
     @recipe = Recipe.find(params[:id])
   end
 
-  private
 
-  def search
-    ingredients = params[:ingredients]
 
-    api_key = ENV['SPOONACULAR_ACCESS_TOKEN']
-    url = URI("https://api.spoonacular.com/recipes/findByIngredients?ingredients=#{ingredients}&number=10&ranking=1&apiKey=#{api_key}")
+  # def search
+  #   ingredients = params[:ingredients]
 
-    response = Net::HTTP.get_response(url)
-    if response.is_a?(Net::HTTPSuccess)
-      @recipes = JSON.parse(response.body)
-    else
-      @recipes = []
-      flash[:alert] = "Failed to fetch recipes. Please try again later."
-    end
+  #   api_key = ENV['SPOONACULAR_ACCESS_TOKEN']
+  #   url = URI("https://api.spoonacular.com/recipes/findByIngredients?ingredients=#{ingredients}&number=10&ranking=1&apiKey=#{api_key}")
 
-    render :index
-  end
+  #   response = Net::HTTP.get_response(url)
+  #   if response.is_a?(Net::HTTPSuccess)
+  #     @recipes = JSON.parse(response.body)
+  #   else
+  #     @recipes = []
+  #     flash[:alert] = "Failed to fetch recipes. Please try again later."
+  #   end
+
+  #   render :index
+  # end
 
   def recipe_params
-    params.require(:recipe).permit(:ingredients, :name) 
+    params.require(:recipe).permit(:ingredients, :name)
   end
 end
--- config/initializers/spoonacular.rb
+++
@@ -1,3 +0,0 @@
-# Spoonacular.configure do |config|
-#   config.access_token = ENV.fetch("SPOONACULAR_ACCESS_TOKEN")
-# end
