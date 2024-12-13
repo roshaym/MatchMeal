6703db6e2101994b0e5dedc62ba854ba1d2bff13
Esha Singh <singh.esha99999@gmail.com>
2024-12-13 12:07:09 +0100

    display recipe and details

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,17 +28,54 @@ puts "Created 3 searches for the user"
 
 searches = Search.all
 
-5.times do
+image_urls = [
+  "http://chelseawinter.co.nz/wp-content/uploads/2013/09/Fish-Dish-Desat-LensTitlt-Sharpen-light-large.jpg",
+  "http://img.taste.com.au/QaDKlckA/taste/2016/11/fresh-summer-vegetable-salad-91664-1.jpeg",
+  "https://static.fanpage.it/wp-content/uploads/sites/22/2018/06/istock-529669952.jpg",
+  "https://cdn.cnn.com/cnnnext/dam/assets/210211142532-18-classic-italian-dishes-super-tease.jpg",
+  "https://wallpapercave.com/wp/wp4820451.jpg"
+]
+
+recipe_details = [
+  {
+    name: "Grilled Fish with Lemon Butter Sauce",
+    ingredients: "Fish, Lemon, Butter, Garlic, Parsley",
+    description: "A delightful grilled fish recipe served with a rich lemon butter sauce, perfect for seafood lovers."
+  },
+  {
+    name: "Fresh Summer Vegetable Salad",
+    ingredients: "Lettuce, Cherry Tomatoes, Cucumber, Bell Peppers, Olive Oil",
+    description: "A vibrant and refreshing summer salad with fresh vegetables and a light dressing."
+  },
+  {
+    name: "Rustic Italian Bruschetta",
+    ingredients: "Bread, Tomatoes, Basil, Olive Oil, Garlic",
+    description: "Crispy toasted bread topped with a fresh tomato and basil mixture, an Italian classic."
+  },
+  {
+    name: "Classic Spaghetti Carbonara",
+    ingredients: "Spaghetti, Eggs, Pancetta, Parmesan Cheese, Black Pepper",
+    description: "A rich and creamy pasta dish featuring pancetta and a Parmesan-based sauce."
+  },
+  {
+    name: "Chocolate Lava Cake",
+    ingredients: "Dark Chocolate, Butter, Sugar, Eggs, Flour",
+    description: "A decadent dessert with a gooey molten chocolate center that melts in your mouth."
+  }
+]
+
+5.times do |i|
   Recipe.create!(
-    name: Faker::Food.dish,
-    description: Faker::Food.description,
-    rating: rand(1..5),
-    ingredients: Faker::Food.ingredient,
-    cooking_time: rand(10..120),
+    name: recipe_details[i][:name],
+    description: recipe_details[i][:description],
+    rating: rand(3..5), # Higher range for better recipes
+    ingredients: recipe_details[i][:ingredients],
+    cooking_time: rand(20..60), # Realistic cooking times
     search_id: searches.sample.id,
-    image: Faker::LoremFlickr.image(size: "300x300", search_terms: ['food'])
+    image: image_urls[i]
   )
 end
+
 
 puts "Created 5 recipes"
 
