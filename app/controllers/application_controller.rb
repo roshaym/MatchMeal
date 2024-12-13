76530d5eaa86827e1ed4fd412a1bb70ff863f6ef
Yahia Jabeur <yahia.jabeur@gmail.com>
2024-12-13 16:32:40 +0100

    refinement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  # Redirect user to their profile page after successful sign up
-  def after_sign_up_path_for(resource)
-    puts "Redirecting to: #{user_path(resource)}"
-    user_path(resource)
-  end
+  # # Redirect user to their profile page after successful sign up
+  # def after_sign_up_path_for(resource)
+  #   puts "Redirecting to: #{user_path(resource)}"
+  #   user_path(resource)
+  # end
 
-
-  # optional, but I leave it here for now
-  def after_sign_in_path_for(resource)
-    user_path(resource)
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redirects to the sign-in page
   end
 end
