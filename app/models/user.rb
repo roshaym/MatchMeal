89089c022b6723fd099894e5dabab82d9e1b411d
Charlotte Lichter <celichter@gmail.com>
2024-12-17 11:40:54 +0100

    updating profile works now

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_picture
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  # Validating password only when it's provided
   validates :password, confirmation: true, length: { minimum: 6 }, allow_nil: true
 
-  validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true
+  has_secure_password
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  def update_without_password(params)
+    params.delete(:password)
+    params.delete(:password_confirmation)
+    self.assign_attributes(params)
+    save(validate: false)
+  end
 end
