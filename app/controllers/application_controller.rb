9175258caefccd0b7f7f25f2592b78a0e3d89ae4
Yahia Jabeur <yahia.jabeur@gmail.com>
2024-12-19 16:36:27 +0100

    fix sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   # # Redirect user to their profile page after successful sign up
   # def after_sign_up_path_for(resource)
@@ -9,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path # Redirects to the sign-in page
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
