0d6fd6d611a095e9be6fed5c6e5353dc7360e771
MATT ROSHAY <ROSHAYM@GMAIL.COM>
2024-12-19 17:32:31 +0100

    amended detect ingredients page and completed checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path # Redirects to the sign-in page
   end
+
+  def default_url_options
+    { host: ENV[“DOMAIN”] || “localhost:3000” }
+  end
+
 end
